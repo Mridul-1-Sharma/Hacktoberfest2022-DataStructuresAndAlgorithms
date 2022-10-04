b2547283ad670dba49864f6722edeb65b78078a9
AJAY <74255983+AJTEACH@users.noreply.github.com>
2022-10-04 17:28:58 +0530

    Update 2sum.cpp

--- Cpp/2sum.cpp
+++ Cpp/2sum.cpp
@@ -1,3 +1,10 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+// You may assume that each input would have exactly one solution, and you may not use the same element twice.
+// You can return the answer in any order.
+
 class Solution {
 public:
     vector<int> twoSum(vector<int> &numbers, int target)
@@ -19,3 +26,15 @@ public:
         return result;
     }
 };
+
+int main() {
+    vector<int>nums = {2, 7, 11, 15};
+    int target = 9;
+    Solution obj;
+    vector<int>ans = obj.twoSum(nums, target);
+    for (auto i : ans) {
+        cout << i << " ";
+    }
+    cout << "\n";
+    return 0;
+}
