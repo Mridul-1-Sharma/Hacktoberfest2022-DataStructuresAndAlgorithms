29f70b60811a089086878cff8207cce5f45531d5
khageshwor <khageshwarjoshi03@gmail.com>
2022-10-05 17:20:46 +0545

    Add: reverse linked list solution

---
+++ python/linked list/reverse_linkedlist.py
@@ -0,0 +1,11 @@
+def reverseList(head):
+    p1, p2 = None, head
+
+    while p2 is not None:
+
+        p3 = p2.next
+        p2.next = p1
+        p1 = p2
+        p2 = p3
+        
+    return p1
