eb7bac3847232fe4e7d30f58ba3daf40a11c0a8e
Tirthankar Ghosh <55987952+tirthadagr8@users.noreply.github.com>
2022-10-06 11:41:05 +0530

    Update Rat_In_A_Maze.cpp

--- Cpp/Competitive programming/Rat_In_A_Maze.cpp
+++ Cpp/Competitive programming/Rat_In_A_Maze.cpp
@@ -1,3 +1,6 @@
+#include<bits/stdc++.h>
+using namespace std;
+
 class Solution{
     public:
     vector<string> ans;
@@ -24,3 +27,26 @@ class Solution{
         return ans;
     }
 };
+int main() {
+    int t;
+    cin >> t;
+    while (t--) {
+        int n;
+        cin >> n;
+        vector<vector<int>> m(n, vector<int> (n,0));
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                cin >> m[i][j];
+            }
+        }
+        Solution obj;
+        vector<string> result = obj.findPath(m, n);
+        sort(result.begin(), result.end());
+        if (result.size() == 0)
+            cout << -1;
+        else
+            for (int i = 0; i < result.size(); i++) cout << result[i] << " ";
+        cout << endl;
+    }
+    return 0;
+}
