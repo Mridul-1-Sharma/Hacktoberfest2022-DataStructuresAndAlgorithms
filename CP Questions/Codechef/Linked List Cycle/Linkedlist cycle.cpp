eaf7f66d5d1c1d965036f0a2ba33c54c367829b6
Alabhya Goel <goelalabhya@gmail.com>
2022-10-06 21:01:12 +0530

    Added LinkedList Cycle Program

---
+++ CP Questions/Codechef/Linked List Cycle/Linkedlist cycle.cpp
@@ -0,0 +1,21 @@
+// Problem Link : https://leetcode.com/problems/linked-list-cycle/
+
+class Solution {
+public:
+	ListNode* middleNode(ListNode* head) {
+    	int n = 0;
+    	ListNode* temp = head;
+    	while(temp) {
+        	n++;
+        		temp = temp->next;
+    	}
+   	 
+    	temp = head;
+   	 
+    	for(int i = 0; i < n / 2; i++) {
+        		temp = temp->next;
+    	}
+   	 
+    	return temp;
+	}
+};
