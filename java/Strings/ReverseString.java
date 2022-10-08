75e0d2fe7def6f1c170e9c5f9d8965468110e1e3
Lakshay Tyagi <tyagilakshay119@gmail.com>
2022-10-08 06:31:30 +0530

    Add ReverseString.java

---
+++ java/Strings/ReverseString.java
@@ -0,0 +1,17 @@
+ public class Reverse   
+{    
+    public static void main(String[] args) {    
+        String string = "Dream big";    
+        //Stores the reverse of given string    
+        String reversedStr = "";    
+            
+        //Iterate through the string from last and add each character to variable reversedStr    
+        for(int i = string.length()-1; i >= 0; i--){    
+            reversedStr = reversedStr + string.charAt(i);    
+        }    
+            
+        System.out.println("Original string: " + string);    
+        //Displays the reverse of given string    
+        System.out.println("Reverse of given string: " + reversedStr);    
+    }    
+}    
