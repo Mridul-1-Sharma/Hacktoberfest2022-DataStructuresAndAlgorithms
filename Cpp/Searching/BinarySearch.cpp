44e981787e2caeb7d8207bbc2cc308a4ad42bc2e
Aradhya Garg <78140726+Aradhyagarg@users.noreply.github.com>
2022-10-08 10:40:11 +0530

    Update BinarySearch.cpp

--- Cpp/Searching/BinarySearch.cpp
+++ Cpp/Searching/BinarySearch.cpp
@@ -1,64 +1,26 @@
-#include <iostream>
+#include<iostream>
+#include<cstdio>
+#include<cmath>
 using namespace std;
-
-int binarySearch(int arr[], int n, int key)
-{
-    int s = 0;
-    int e = n;
-
-    while (s <= e)
-    {
-        int mid = (s + e) / 2;
-
-        if (arr[mid] == key)
-        {
-            return mid;
-        }
-
-        else if (arr[mid] > key)
-        {
-            e = mid - 1;
-        }
-
-        else
-        {
-            s = mid + 1;
-        }
-    }
-
-    return -1;
+int rec_binary_search(int arr[], int left, int right, int x) {
+  int result;
+  if (right >= left) {
+    int mid = left + (right - left)/2;
+    if (arr[mid] == x)  return mid;
+    if (arr[mid] > x) return rec_binary_search(arr, left, mid-1, x);
+    result = rec_binary_search(arr, mid+1, right, x);
+    return result;
+  }
+  return -1;       // when element is not present in array.
 }
 
-int main()
-{
-    int n;
-    int iRet = 0;
-    cout<<"Enter size of array: -"<<endl;
-    cin >> n;
-
-    int arr[n];
-   
-    for (int i = 0; i < n; i++)
-    {
-	  cout<<"Enter "<<i + 1<<" Element in array: -"<<endl;
-        cin>>arr[i];
-    }
-
-    int key;
-    cout<<"Enter element to search in array: -"<<endl;
-    cin >> key;
-     
-
-    iRet = binarySearch(arr, n, key);
-
-    if(iRet == -1)
-    {
-	cout<<"Element is not present in the array !"<<endl;
-    }
-    else
-    {
-	cout<<"Element is present in the array !"<<endl;
-    }
-
-    return 0;
+int main() {
+  int loc,x,array[]={10,11,12,13,14,25,26,37,48,59};
+  x=11;        // element to be searched in the array
+  loc=rec_binary_search(array,0,10,x);
+  if(loc != -1)
+    cout<<"Element found at location : "<<loc;
+  else
+    cout<<"Element not present in the array.";
+  return 0;
 }
