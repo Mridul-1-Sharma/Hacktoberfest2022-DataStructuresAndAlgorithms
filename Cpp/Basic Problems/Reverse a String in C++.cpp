def370d6e2e7126398bf70f344c535a20077fb8d
SHIVA MANI <86513644+ShivaMani02@users.noreply.github.com>
2022-10-09 16:23:40 +0530

    Create Reverse a String in C++.cpp

---
+++ Cpp/Basic Problems/Reverse a String in C++.cpp
@@ -0,0 +1,25 @@
+// C++ program to reverse a string
+// using first to last approach
+// 'for' loop
+#include <bits/stdc++.h>
+using namespace std;
+
+// Function to reverse a string
+void reverseStr(string& str)
+{
+	int n = str.length();
+
+	// Swap character starting from two
+	// corners
+	for (int i = 0; i < n / 2; i++)
+		swap(str[i], str[n - i - 1]);
+}
+
+// Driver program
+int main()
+{
+	string str = "geeksforgeeks";
+	reverseStr(str);
+	cout << str;
+	return 0;
+}
